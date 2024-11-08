121a675eac5fd3517cfbf86890913fcc827fdf89
Parvez-pabel <pabelalomparvez2233@gmail.com>
2024-11-09 01:33:25 +0600

    page router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/project" element={<Project />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
